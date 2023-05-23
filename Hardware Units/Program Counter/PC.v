8dda729c36a3421d69071f9ff0898b1e6c8d66e5
kavindax <e18170@eng.pdn.ac.lk>
2023-05-23 15:28:17 +0530

    new PC module added

---
+++ Hardware Units/Program Counter/PC.v
@@ -0,0 +1,20 @@
+module programeCounter(CLK,RESET,mux_output,PC,busyWait);
+input CLK,RESET,busyWait;
+input [31:0] mux_output;
+output [31:0] PC;
+reg PC;
+
+
+always @(RESET) begin   // To reset the PC
+	    #1                               //pc write delay
+		PC = -4;                      //initial pc value assigned at reset
+	end
+	
+	
+always @(posedge CLK) begin   
+    #1                            //pc write delay
+    if(!busyWait)begin            //update the pc only when busywait is 0
+        PC = mux_output;          //pc updated with new value
+    end
+end
+endmodule
